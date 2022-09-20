4933f01d77a5054093421894434eeda48c840a11
Serg Bogomolov <serg@bogomolov.su>
2022-09-20 22:59:58 +0300

    Lesson10

--- core-service/src/main/java/com/geekbrains/coreservice/controllers/OrderController.java
+++ core-service/src/main/java/com/geekbrains/coreservice/controllers/OrderController.java
@@ -1,25 +1,33 @@
 package com.geekbrains.coreservice.controllers;
 
 import com.geekbrains.coreservice.converters.OrderConverter;
-import com.geekbrains.coreservice.dto.OrderDetailsDto;
 import com.geekbrains.coreservice.dto.OrderDto;
 import com.geekbrains.coreservice.services.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
+@Tag(name="OrderController", description="OrderController")
 public class OrderController {
+        @Schema(description = "orderService")
         private final OrderService orderService;
+        @Schema(description = "orderConverter")
         private final OrderConverter orderConverter;
 
 
     @GetMapping
+    @Operation(summary = "getCurrenUrders")
     public List<OrderDto> getCurrenUrders(@RequestHeader String username){
         return orderService.findOrdersByUsername(username).stream()
                 .map(orderConverter::entityToDto).collect(Collectors.toList());
--- core-service/src/main/java/com/geekbrains/coreservice/controllers/ProfileController.java
+++ core-service/src/main/java/com/geekbrains/coreservice/controllers/ProfileController.java
@@ -1,6 +1,8 @@
 package com.geekbrains.coreservice.controllers;
 
 import com.geekbrains.coreservice.dto.ProfileDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/profile")
+@Tag(name="ProfileController", description="ProfileController")
 public class ProfileController {
     @GetMapping
+    @Operation(summary = "getCurrentUserInfo")
     public ProfileDto getCurrentUserInfo(@RequestHeader String username) {
         return new ProfileDto(username);
     }
